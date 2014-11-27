ff569378d835685d2d1aecfedbfbdd0f45f19786
Klaas Bosteels <klaas.bosteels@gmail.com>
2014-11-27 13:21:35 +0100

    Use custom RPM for Python 2.7

--- python27/init.sh
+++ python27/init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-yum install -y python27 python27-devel python27-setuptools
+mkdir /root/python27 2> /dev/null
+pushd /root/python27
+
+wget https://github.com/klbostee/spark-ec2/releases/download/v4.1/python27-2.7.8.klbostee-1.x86_64.rpm
+wget http://peak.telecommunity.com/dist/ez_setup.py
+
+yum install -y python27-2.7.8.klbostee-1.x86_64.rpm
+python2.7 ez_setup.py setuptools
+
+popd
--- python27/setup.sh
+++ python27/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+/root/spark-ec2/copy-dir /root/python27
+
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t $SSH_OPTS root@$node 'yum install -y python27 python27-setuptools' & sleep 0.3
+  ssh -t $SSH_OPTS root@$node 'cd /root/python27; yum install -y python27-2.7.8.klbostee-1.x86_64.rpm; python2.7 ez_setup.py setuptools' & sleep 0.3
 done
 wait
+
