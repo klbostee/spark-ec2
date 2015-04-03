b7e18ec333b02c6fe01f4b4d49e9aaa43809a679
Klaas Bosteels <klaas.bosteels@gmail.com>
2015-04-03 17:38:08 +0200

    Switch back to standard python27 from AWS

--- python27/init.sh
+++ python27/init.sh
@@ -3,10 +3,7 @@
 mkdir /root/python27 2> /dev/null
 pushd /root/python27
 
-wget https://github.com/klbostee/spark-ec2/releases/download/v4.1/python27-2.7.8.klbostee-1.x86_64.rpm
-wget http://peak.telecommunity.com/dist/ez_setup.py
-
-yum install -y python27-2.7.8.klbostee-1.x86_64.rpm
+yum install -y python27 python27-devel
 python2.7 ez_setup.py setuptools
 
 popd
