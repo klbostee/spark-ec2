0fed07f8b7dfbed9febe7fc95323ea44187caf89
Klaas Bosteels <klaas.bosteels@gmail.com>
2015-04-03 18:06:57 +0200

    Fix buggy switch to AWS' python27

--- python27/init.sh
+++ python27/init.sh
@@ -3,6 +3,8 @@
 mkdir /root/python27 2> /dev/null
 pushd /root/python27
 
+wget http://peak.telecommunity.com/dist/ez_setup.py
+
 yum install -y python27 python27-devel
 python2.7 ez_setup.py setuptools
 
